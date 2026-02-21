16a8a499fbf31ea573d6031feca13d5c6c0c9ce6
YusufErdemK <yusuferdemyek@gmail.com>
2026-02-21 08:31:31 +0300

    it has been made compatible with the dark theme feature

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'services/storage_service.dart';
 import 'providers/tree_provider.dart';
 import 'providers/transaction_provider.dart';
+import 'providers/settings_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
         ChangeNotifierProvider(create: (_) => TreeProvider()),
         ChangeNotifierProvider(
           create: (context) {
@@ -29,18 +31,33 @@ class MyApp extends StatelessWidget {
           },
         ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Adiyok - Ağaç & Gelir/Gider Yönetimi',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF2D6A4F),
-            brightness: Brightness.light,
-          ),
-          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-        ),
-        home: const HomeScreen(),
+      child: Consumer<SettingsProvider>(
+        builder: (context, settings, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Adiyok - Ağaç & Gelir/Gider Yönetimi',
+            themeMode: settings.themeMode,
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color(0xFF2D6A4F),
+                brightness: Brightness.light,
+              ),
+              textTheme: GoogleFonts.poppinsTextTheme(),
+            ),
+            darkTheme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color(0xFF2D6A4F),
+                brightness: Brightness.dark,
+              ),
+              textTheme: GoogleFonts.poppinsTextTheme(
+                ThemeData(brightness: Brightness.dark).textTheme,
+              ),
+            ),
+            home: const HomeScreen(),
+          );
+        },
       ),
     );
   }
