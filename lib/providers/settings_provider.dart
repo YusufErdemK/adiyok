6ee9f39b723ca506ebce2c19a2ad98e3b240d389
YusufErdemK <yusuferdemyek@gmail.com>
2026-03-05 21:55:57 +0300

    settings: groq token system

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -3,10 +3,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
   static const String _darkModeKey = 'dark_mode';
+  static const String _aiEnabledKey = 'ai_enabled';
+  static const String _groqApiKeyKey = 'groq_api_key';
 
   bool _darkMode = false;
+  bool _aiEnabled = false;
+  String _groqApiKey = '';
 
   bool get darkMode => _darkMode;
+  bool get aiEnabled => _aiEnabled;
+  String get groqApiKey => _groqApiKey;
   ThemeMode get themeMode => _darkMode ? ThemeMode.dark : ThemeMode.light;
 
   SettingsProvider() {
@@ -16,6 +22,8 @@ class SettingsProvider extends ChangeNotifier {
   Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
     _darkMode = prefs.getBool(_darkModeKey) ?? false;
+    _aiEnabled = prefs.getBool(_aiEnabledKey) ?? false;
+    _groqApiKey = prefs.getString(_groqApiKeyKey) ?? '';
     notifyListeners();
   }
 
@@ -24,5 +32,19 @@ class SettingsProvider extends ChangeNotifier {
     notifyListeners();
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_darkModeKey, value);
+  }
+
+  Future<void> setAiEnabled(bool value) async {
+    _aiEnabled = value;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_aiEnabledKey, value);
+  }
+
+  Future<void> setGroqApiKey(String value) async {
+    _groqApiKey = value;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_groqApiKeyKey, value);
   }
 }
